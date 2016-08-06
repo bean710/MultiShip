5a98bf260c803d0aae7d37d6137a85152a12f927
bean710 <keener4christ@gmail.com>
2016-08-06 12:48:04 -0400

    Changed IP to network-wide

--- core/src/com/bean710/multiship/dontshowonstream/IP.java
+++ core/src/com/bean710/multiship/dontshowonstream/IP.java
@@ -1,7 +1,7 @@
 package com.bean710.multiship.dontshowonstream;
 
 public class IP {
-	private static String ip = "http://localhost:3000"; //67.234.172.5
+	private static String ip = "http://192.168.2.123:3000"; //67.234.172.5
 	
 	public static String getIp() {
 		return ip;
